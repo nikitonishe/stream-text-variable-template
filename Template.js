f26e1e03a96bee9b7b6012011d90ea57d0ee35dc
Constantin Tsukanov <c@indotech.ru>
2017-04-15 16:09:56 +0300

    Make class, add .eslintrc

---
+++ Template.js
@@ -0,0 +1,42 @@
+const { Transform } = require('stream');
+class Template extends Transform {
+  constructor(props = {}, options) {
+    super(options);
+    this.pieces = '';
+    if (props) this.props = props;
+    this.test = {
+      start: /\{\{/,
+      block: /\{\{([^{}]+?)\}\}/
+    }
+  }
+
+  _hasStart() {
+    return this.test.start.test(this.pieces);
+  }
+
+  _hasBlock() {
+    return this.test.block.test(this.pieces);
+  }
+
+  _compile() {
+    const match = this.pieces.match(this.test.block);
+    if (!match) throw new Error('Unstable variable');
+    const field = match[1];
+    const toReplace = match[0];
+    if (!(this.props && this.props[field])) throw new Error('Variable ' + field + ' is not defined');
+    this.pieces = this.pieces.replace(toReplace, this.props[field]);
+  }
+
+  _transform(chunk, enc, callback) {
+    this.pieces += chunk.toString();
+    if (this._hasStart()) {
+      if (!this._hasBlock()) return callback();
+      while (this._hasBlock()) {
+        this._compile();
+      }
+      this.push(this.pieces);
+    }
+  }
+}
+
+module.exports = Template;
